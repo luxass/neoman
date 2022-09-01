a86715bda786e0f053ab8b1d0efd50e74cbe2de1
luxass <lucasnrgaard@gmail.com>
2022-09-01 20:39:50 +0200

    chore: changes createEnvironment

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,8 @@ import { NeomanContext } from './types';
 
 export { NeomanGenerator } from './types';
 
-export const createEnvironment = (context: NeomanContext) => {
-  const env = new NeomanEnvironment(context);
-
+export const createEnvironment = (context?: NeomanContext) => {
+  const env = new NeomanEnvironment(context || {});
   return env;
 };
 
